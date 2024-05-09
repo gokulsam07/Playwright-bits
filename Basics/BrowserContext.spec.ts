6707a166c0c057890ffe921a37a606c4b1dc1dbb
gokulsam07 <gokuls2381@gmail.com>
2024-05-10 00:42:55 +0530

    Browser context, execute from local

---
+++ Basics/BrowserContext.spec.ts
@@ -0,0 +1,34 @@
+import {test, expect, Browser, Page, chromium,Locator,Expect, BrowserContext} from '@playwright/test';
+import { ChromiumBrowser } from '@playwright/test';
+
+test('login test',async({}, testInfo)=>{
+    testInfo.setTimeout(60000);
+    const browser:Browser = await chromium.launch({headless:false,executablePath:"C:/Program Files/Google/Chrome/Application/chrome.exe"});
+    //this will run in the version available in the path mentioned
+    const context1:BrowserContext = await browser.newContext();
+    const context2:BrowserContext = await browser.newContext();
+    const page1:Page=await context1.newPage();
+    const page2:Page=await context2.newPage();
+    await page1.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page1.waitForLoadState('networkidle');
+    const uName:Locator = await page1.locator("css=input[placeholder='Username']");
+    await uName.fill("Admin");
+    const password:Locator = await page1.locator("css=input[placeholder='Password']");
+    await password.fill("admin123");
+    const login: Locator = await page1.locator("css=button[type='submit']");
+    await login.click();
+    const alertMessage1 = await page1.locator("css=.oxd-userdropdown-tab");
+    expect(await alertMessage1.waitFor({ state: 'visible' }));
+
+    await page2.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page2.waitForLoadState('networkidle');
+    const uName2:Locator = await page2.locator("css=input[placeholder='Username']");
+    await uName2.fill("admin1");
+    const password2:Locator = await page2.locator("css=input[placeholder='Password']");
+    await password2.fill("Admin123");
+    const login2: Locator = await page2.locator("css=button[type='submit']");
+    await login2.click();
+    const alertMessage = await page2.locator("css=.oxd-text.oxd-text--p.oxd-alert-content-text");
+    await alertMessage.waitFor({ state: 'visible' });
+    expect(await alertMessage.textContent()).toBe('Invalid credentials');
+});
---
+++ Basics/Login.spec.ts
@@ -0,0 +1,20 @@
+import {test, expect, Browser, Page, chromium,Locator,Expect} from '@playwright/test';
+import { ChromiumBrowser } from '@playwright/test';
+import { channel } from 'diagnostics_channel';
+
+
+test('login test',async()=>{
+    const browser:Browser = await chromium.launch({headless:false,channel:"chrome"}); //to run in locally available browser run channel 
+    const page:Page=await browser.newPage();
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.waitForLoadState('networkidle');
+    const uName:Locator = await page.locator("css=input[placeholder='Username']");
+    await uName.fill("admin");
+    const password:Locator = await page.locator("css=input[placeholder='Password']");
+    await password.fill("Admin123");
+    const login: Locator = await page.locator("css=button[type='submit']");
+    await login.click();
+    const title:String = await page.title();
+    console.log(title);
+    expect(title).toBe('OrangeHRM');
+});
