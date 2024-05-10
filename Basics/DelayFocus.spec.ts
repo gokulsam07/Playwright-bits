8f3539d486a5ed636a93d95190ae045c7c93a83b
gokulsam07 <gokuls2381@gmail.com>
2024-05-10 10:27:32 +0530

    Focus, SequentialFill & storageState

---
+++ Basics/DelayFocus.spec.ts
@@ -0,0 +1,26 @@
+import {test, expect, Browser, Page, chromium,Locator,Expect} from '@playwright/test';
+import { ChromiumBrowser } from '@playwright/test';
+import { channel } from 'diagnostics_channel';
+
+
+test('delay and focus test',async()=>{
+    const browser:Browser = await chromium.launch({headless:false,channel:"chrome"}); 
+    const page:Page=await browser.newPage();
+    await page.goto('https://flipkart.com');
+    await page.waitForLoadState('networkidle');
+    await page.getByPlaceholder('Search for Products, Brands and More').pressSequentially('Macbook Air',{delay:500});
+    await page.waitForTimeout(2000);
+});
+
+
+
+test('focus test',async()=>{
+    const browser:Browser = await chromium.launch({headless:false,channel:"chrome"}); 
+    const page:Page=await browser.newPage();
+    await page.goto('https://facebook.com');
+    await page.waitForLoadState('networkidle');
+    const name = await page.locator('id=pass');
+    await name.focus();
+    await name.fill("gokuls2381@gmail.com");
+    await page.waitForTimeout(2000);
+});
---
+++ Basics/ReuseSeesion.spec.ts
@@ -0,0 +1,30 @@
+import {test, expect, Browser,BrowserContext, Page, chromium,Locator,Expect} from '@playwright/test';
+import { ChromiumBrowser } from '@playwright/test';
+import { channel } from 'diagnostics_channel';
+import path from 'path';
+
+test('login test',async()=>{
+    const browser:Browser = await chromium.launch({headless:false,channel:"chrome"});
+    const context:BrowserContext = await browser.newContext();
+    const page:Page=await context.newPage();
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.waitForLoadState('networkidle');
+    const uName:Locator = await page.getByPlaceholder("Username");
+    await uName.fill("Admin");
+    const password:Locator = await page.getByPlaceholder("Password");
+    await password.fill("admin123");
+    const login: Locator = await page.locator("css=button[type='submit']");
+    await login.click();
+    await context.storageState({path:"./loginAuth.json"});
+    await page.waitForTimeout(4000);
+});
+
+test('login without login page',async()=>{
+    const browser:Browser = await chromium.launch({headless:false,channel:"chrome"});
+    const context:BrowserContext = await browser.newContext({storageState:"./loginAuth.json"});
+    const page:Page=await context.newPage();
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.waitForLoadState('networkidle');
+    expect(page.locator("//h6[normalize-space()='Dashboard']")).toBeVisible();
+    await page.waitForTimeout(4000);
+});
