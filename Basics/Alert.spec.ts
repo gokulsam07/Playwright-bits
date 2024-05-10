090a446ee8ad197422f026e0db273afa32e2d094
gokulsam07 <gokuls2381@gmail.com>
2024-05-10 13:14:20 +0530

    Input & Alert

---
+++ Basics/Alert.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect, Browser, Page, chromium, Locator, Expect } from '@playwright/test';
+import { ChromiumBrowser } from '@playwright/test';
+import { channel } from 'diagnostics_channel';
+
+
+test('alert test', async () => {
+    const browser: Browser = await chromium.launch({ headless: false, channel: "chrome" });
+    const page: Page = await browser.newPage();
+    await page.goto('https://letcode.in/alert');
+    await page.waitForLoadState('domcontentloaded');
+    await page.waitForTimeout(1000);
+    page.on('dialog', dialog => {
+        console.log(dialog.message()); //handle events before click
+        dialog.accept("Hello world!");
+    });
+    await page.getByText("Prompt Alert").click();
+    await page.waitForTimeout(2000);
+    await expect(page.locator("#myName")).toContainText("Hello world");
+    await page.waitForTimeout(1000);
+    await page.reload();
+    await page.waitForLoadState('domcontentloaded');
+    await page.getByText("Simple Alert").click(); // don't handle dialog twice
+});
---
+++ Basics/Input.spec.ts
@@ -0,0 +1,20 @@
+import {test, expect, Browser, Page, chromium,Locator,Expect} from '@playwright/test';
+import { ChromiumBrowser } from '@playwright/test';
+import { channel } from 'diagnostics_channel';
+
+
+test('login test',async()=>{
+    const browser:Browser = await chromium.launch({headless:false,channel:"chrome"}); 
+    const page:Page=await browser.newPage();
+    await page.goto('https://letcode.in/edit');
+    await page.waitForLoadState('domcontentloaded');
+    await page.locator("id=fullName").fill("admin"); //fill will clear the input field & type in the value
+    await page.waitForTimeout(2000);
+    const fill =  await page.locator("id=join");
+    await fill.focus(); 
+    await page.keyboard.press("End");
+    await fill.pressSequentially(" Human");
+    await page.waitForTimeout(2000);
+    await page.locator("id=clearMe").clear();
+    await page.waitForTimeout(2000);
+});
