65829ec6a249aae7760ce5fbf9a2eb365c8ec70d
gokulsam07 <gokuls2381@gmail.com>
2024-05-11 14:43:42 +0530

    Long Press

--- Basics/MouseActions.spec.ts
+++ Basics/MouseActions.spec.ts
@@ -18,7 +18,7 @@ test('right test', async () => {
     const page: Page = await context.newPage();
     await page.goto('https://www.demo.guru99.com/test/simple_context_menu.html');
     await page.waitForLoadState('networkidle');
-    await page.getByText("right click me").click({button:'right'});
+    await page.getByText("right click me").click({ button: 'right' });
     await page.waitForTimeout(5000);
 });
 
@@ -28,6 +28,20 @@ test('shift test', async () => {
     const page: Page = await context.newPage();
     await page.goto('https://the-internet.herokuapp.com/shifting_content');
     await page.waitForLoadState('networkidle');
-    await page.getByText("Example 1: Menu Element").click({modifiers:["Shift"]});
+    await page.getByText("Example 1: Menu Element").click({ modifiers: ["Shift"] });
     await page.waitForTimeout(5000);
 });
+
+test('click and hold test', async () => {
+    const browser: Browser = await chromium.launch({ headless: false, channel: "chrome" });
+    const context: BrowserContext = await browser.newContext();
+    const page: Page = await context.newPage();
+    await page.goto('https://letcode.in/buttons');
+    await page.waitForLoadState();
+    const button = await page.waitForSelector("css=button[id='isDisabled'] div h2");
+    //await page.click("css=button[id='isDisabled'] div h2",{delay:5000});
+    //or
+    await button.scrollIntoViewIfNeeded();
+    await button.click({ delay: 5000 });
+});
+
