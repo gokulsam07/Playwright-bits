a0b6e8dce018b98e0828216cc6243cf7b92a90fa
gokulsam07 <gokuls2381@gmail.com>
2024-08-01 01:49:29 +0530

    mock intercept network

--- Basics/Alert.spec.ts
+++ Basics/Alert.spec.ts
@@ -1,24 +1,40 @@
-import { test, expect, Browser, Page, chromium, Locator, Expect } from '@playwright/test';
+import { test, expect, Browser, Page, chromium, Locator, Expect, BrowserContext } from '@playwright/test';
 import { ChromiumBrowser } from '@playwright/test';
+import assert from 'assert';
 import { channel } from 'diagnostics_channel';
 
 
 test('alert test', async () => {
+    test.setTimeout(120000);
     const browser: Browser = await chromium.launch({ headless: false, channel: "chrome" });
-    const page: Page = await browser.newPage();
-    await page.goto('https://letcode.in/alert');
+    const cntxt : BrowserContext =  await browser.newContext();
+    await cntxt.clearCookies();
+    await cntxt.clearPermissions();
+    const page: Page = await cntxt.newPage();
+    await page.goto('http://10.194.42.241:8080/D2-Smartview');
     await page.waitForLoadState('domcontentloaded');
-    await page.waitForTimeout(1000);
-    page.on('dialog', dialog => {
-        console.log(dialog.message()); //handle events before click
-        dialog.accept("Hello world!");
-    });
-    await page.getByText("Prompt Alert").click();
-    await page.waitForTimeout(2000);
+    await page.locator(".input-username").fill("previewuser");
+    await page.locator(".input-password").fill("Password@1234567");
+    await page.locator("select[aria-label='Select a repository']").selectOption({ value: 'testenv' });
+    await page.locator(".login-btn.btn-submit").click();
+  //  await page.waitForTimeout(15000)
+  await page.route('**/*', route => {
+    const url = route.request().url();
+    const resourceType = route.request().resourceType();
+    if ((resourceType=== 'fetch'|| resourceType==='xhr') && url.includes('ctfConfig.json')) {
+      console.log(`Blocking request to: ${url}`);
+      route.abort();  
+    } else {
+      route.continue();
+    }
+  });
+  await page.waitForSelector("a[title='Profile Menu']");
+    // await page.getByText("Prompt Alert").click();
+    // await page.waitForTimeout(2000);
     await expect(page.locator("#myName")).toContainText("Hello world");
-    await page.waitForTimeout(1000);
-    await page.reload();
-    await page.waitForLoadState('domcontentloaded');
-    await page.getByText("Simple Alert").click(); // don't handle dialog twice
-    await browser.close();
+    // await page.waitForTimeout(1000);
+    // await page.reload();
+    // await page.waitForLoadState('domcontentloaded');
+    // await page.getByText("Simple Alert").click(); // don't handle dialog twice
+    // await browser.close();
 });
