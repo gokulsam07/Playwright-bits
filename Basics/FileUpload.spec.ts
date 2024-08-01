52b45ccc4468e9d868b479df89f0b67be37a3a6a
gokulsam07 <gokuls2381@gmail.com>
2024-08-01 18:32:06 +0530

    file chooser

--- Basics/FileUpload.spec.ts
+++ Basics/FileUpload.spec.ts
@@ -23,3 +23,21 @@ test('file upload test mutliple', async () => {
     await page.waitForTimeout(5000);
     await browser.close();
 });
+
+test('d2sv non input file tag upload',async()=>{
+    const browser: Browser = await chromium.launch({ headless: false, channel: "chrome" });
+    const context: BrowserContext = await browser.newContext();
+    const page: Page = await context.newPage();
+    await page.goto('http://10.194.42.241:8080/D2-Smartview/ui/#d2/nodes/0c0000018001ed91');
+    await page.waitForLoadState('domcontentloaded');
+    await page.locator(".input-username").fill("previewuser");
+    await page.locator(".input-password").fill("Password@1234567");
+    await page.locator("select[aria-label='Select a repository']").selectOption({ value: 'testenv' });
+    await page.locator(".login-btn.btn-submit").click()
+    await page.locator("otc-checkbox[title='Select Dummy']").click()
+    const fileChooserPromise = page.waitForEvent('filechooser');
+    await page.locator("//a[normalize-space()='Add version']").click()
+    const fileChooser = await fileChooserPromise;
+    await fileChooser.setFiles('C:\\D2SV\\d2smartview-d2tafcode\\E2ED2UnityTools\\D2UnityAutomation\\stories\\D2Unity\\16.6\\UI\\CheckIn\\Docs\\37615.pdf');
+    await page.waitForTimeout(15000)
+})
